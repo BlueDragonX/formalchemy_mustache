197e3371aa9bdb51730e2063ff5b5af293dd551d
Ryan Bourgeois <bluedragonx@gmail.com>
2012-08-12 22:20:52 -0500

    Added tests for MustacheFieldRenderer.factory method.

--- tests/test_fields.py
+++ tests/test_fields.py
@@ -40,6 +40,10 @@ class TestMustacheFieldRenderer(unittest.TestCase):
         self.directories = [os.path.join(here, 'templates')]
         self.template = 'field'
 
+        self.output_extra = 'some extra data'
+        self.output_expected = "Name: %s-%s-%s\nValue: %s\nExtra: %s\n" % (
+            type(self.model).__name__, self.model.text, 'text', self.model.text, self.output_extra)
+
     def test_init_without_dirs(self):
         """Test the __init__ method without the directories param."""
         configure(self.directories)
@@ -65,12 +69,34 @@ class TestMustacheFieldRenderer(unittest.TestCase):
         
     def test_render(self):
         """Test the render method."""
-        extra = 'some more data'
-        expected = "Name: %s-%s-%s\nValue: %s\nExtra: %s\n" % (
-            type(self.model).__name__, self.model.text, 'text', self.model.text, extra)
         renderer = MustacheFieldRenderer(self.field, self.template,
             self.directories)
-        output = renderer.render(extra=extra)
-        self.assertEqual(output, expected,
+        output = renderer.render(extra=self.output_extra)
+        self.assertEqual(output, self.output_expected,
             'renderer.render is invalid')
+
+    def test_factory_without_dirs(self):
+        """Test the factory method without the directories param."""
+        configure(self.directories)
+        clazz = MustacheFieldRenderer.factory(self.template)
+        renderer = clazz(self.field)
+        self.assertEqual(renderer.field, self.field,
+            'renderer.field is invalid')
+        self.assertEqual(renderer.template, self.template,
+            'renderer.template is invalid')
+        self.assertEqual(renderer.renderer.search_dirs,
+            config.engine.directories,
+            'renderer.directories is invalid')
+
+    def test_factory_with_package(self):
+        """Test the factory method with the directories param."""
+        configure(self.directories)
+        clazz = MustacheFieldRenderer.factory(self.template, self.directories)
+        renderer = clazz(self.field)
+        self.assertEqual(renderer.field, self.field,
+            'renderer.field is invalid')
+        self.assertEqual(renderer.template, self.template,
+            'renderer.template is invalid')
+        self.assertEqual(renderer.renderer.search_dirs, self.directories,
+            'renderer.directories is invalid')
 
