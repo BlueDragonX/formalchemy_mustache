1a6f1e4191d1e8f50a17596bba38874e7c18facc
Ryan Bourgeois <bluedragonx@gmail.com>
2012-08-17 23:40:50 -0500

    Fixed missing import and usage of wrong here variable.

--- tests/base.py
+++ tests/base.py
@@ -8,6 +8,7 @@ Base test case classes.
 
 import os
 import unittest
+import formalchemy_mustache
 from .dummy import DummyModel
 from formalchemy.forms import FieldSet
 from formalchemy.tables import Grid
@@ -25,7 +26,7 @@ class BaseCase(unittest.TestCase):
         package_here = os.path.abspath(
             os.path.dirname(formalchemy_mustache.__file__))
 
-        self.templates = os.path.join(pkgpath, 'templates')
+        self.templates = os.path.join(here, 'templates')
         self.package_templates = [os.path.join(package_here, 'templates')]
 
 
